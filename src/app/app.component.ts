327de37813771ab1f0e08d6c82648d1c08a2ab73
Charles Chow <Charles.Chow@sas.com>
2019-08-02 11:39:17 -0400

    Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'EasyEmail';
+
+  miles = [
+    {value: '10-0', viewValue: '10'},
+    {value: '25-1', viewValue: '25'},
+    {value: '50-2', viewValue: '50'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material';
+
+import { GridsModule } from './grids/grids.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +15,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    //   MatIconRegistry,
+    // ,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
